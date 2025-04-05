f2b81afb3d989c3031712ce7d2733519945cd68d
Kabinchi <kabinchi@yandex.ru>
2025-04-05 12:34:42 +0300

    Add PlaneControl class

---
+++ src/PlaneAppExam/PlaneControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneAppExam
+{
+    public class PlaneControl
+    {
+        private List<Plane> planes = new List<Plane>();
+
+        public void Add()
+        {
+
+        }
+
+        public void Sort()
+        {
+
+        }
+
+        public bool SaveToFile()
+        {
+
+        }
+    }
+}
